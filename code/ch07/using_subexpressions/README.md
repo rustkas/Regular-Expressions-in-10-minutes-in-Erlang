6503f368a46f4786592cd171bfd5dbb35697e132
Anatolii Kosorukov <java1cprog@yandex.ru>
2021-08-04 19:31:58 +0300

    Add Chapte r7 tests

---
+++ code/ch07/using_subexpressions/README.md
@@ -0,0 +1,20 @@
+using_subexpressions
+=====
+
+## Create new project
+
+----	
+	
+	# all commands in one string
+	$ rebar3 new lib using_subexpressions && cd using_subexpressions
+	
+## Get dependencies
+	$ rebar3 deps	
+
+## Format
+	$ rebar3 format
+	
+## EUnit
+-----
+	$ rebar3 eunit
+	$ rebar3 eunit -m using_subexpressions_tests
---
+++ code/ch07/using_subexpressions/test/using_subexpressions_tests.erl
@@ -0,0 +1,108 @@
+% For research For research mode, activate the RESEARCH constant.
+-module(using_subexpressions_tests).
+
+-define(RESEARCH, true).
+
+%%
+%% Tests
+%%
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+% Add commond helper files to the module
+-include("helper.util").
+
+get_file_content() ->
+    String = read_local_file("data" ++ [$/] ++ "nonbreaking spaces.txt"),
+    String.
+
+get_file_ru_content() ->
+    String = read_local_file("data" ++ [$/] ++ "nonbreaking spaces ru.txt"),
+    String.
+
+get_ip_content() ->
+String = read_local_file("data" ++ [$/] ++ "ip.txt"),
+    String.
+	
+get_ip_ru_content()->
+String = read_local_file("data" ++ [$/] ++ "ip ru.txt"),
+    String.
+
+-ifdef(RESEARCH).
+
+reasearch_test() ->
+    Text = get_ip_content(),
+    Regex = "(\\d{1,3}\\.){3}\\d{1,3}",
+    FullResult = re:run(Text, get_mp(Regex), [global, {capture, first, list}]),
+    ?debugFmt("Result = ~p~n", [FullResult]).
+
+reasearch_ru_test() ->
+    Text = get_ip_ru_content(),
+    Regex = "(\\d{1,3}\\.){3}\\d{1,3}",
+    FullResult =
+        re:run(Text, get_mp(Regex, [unicode]), [global, {capture, first, list}]),
+    ?debugFmt("Result = ~p~n", [FullResult]).
+
+-else.
+
+reasearch_01_test() ->
+    Expected = nomatch,
+    Text = get_file_content(),
+    Regex = "&nbsp;{2,}",
+    Result = re:run(Text, get_mp(Regex), [global, {capture, all, list}]),
+    ?assertEqual(Expected, Result).
+	
+reasearch_02_test() ->
+    Expected = [["&nbsp;&nbsp;"]],
+    Text = get_file_content(),
+    Regex = "(&nbsp;){2,}",
+    {match, Result} = re:run(Text, get_mp(Regex), [global, {capture, first, list}]),
+    ?assertEqual(Expected, Result).	
+	
+
+reasearch_01_ru_test() ->
+    Expected = nomatch,
+    Text = get_file_ru_content(),
+    Regex = "&nbsp;{2,}",
+    Result = re:run(Text, get_mp(Regex, [unicode]), [global, {capture, all, list}]),
+    ?assertEqual(Expected, Result).
+	
+reasearch_02_ru_test() ->
+    Expected = [["&nbsp;&nbsp;"]],
+    Text = get_file_ru_content(),
+    Regex = "(&nbsp;){2,}",
+    {match, Result} = re:run(Text, get_mp(Regex, [unicode]), [global, {capture, first, list}]),
+    ?assertEqual(Expected, Result).	
+
+reasearch_ip_01_test() ->
+    Expected = [["12.159.46.200"]],
+    Text = get_ip_content(),
+    Regex = "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}",
+    {match, Result} = re:run(Text, get_mp(Regex), [global, {capture, all, list}]),
+    ?assertEqual(Expected, Result).
+
+reasearch_ip_ru_01_test() ->
+    Expected = [["12.159.46.200"]],
+    Text = get_ip_ru_content(),
+    Regex = "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}",
+    {match, Result} = re:run(Text, get_mp(Regex, [unicode]), [global, {capture, all, list}]),
+    ?assertEqual(Expected, Result).
+
+reasearch_ip_02_test() ->
+    Expected = [["12.159.46.200"]],
+    Text = get_ip_content(),
+    Regex = "(\\d{1,3}\\.){3}\\d{1,3}",
+    {match, Result} = re:run(Text, get_mp(Regex), [global, {capture, first, list}]),
+    ?assertEqual(Expected, Result).
+
+reasearch_ip_ru_02_test() ->
+    Expected = [["12.159.46.200"]],
+    Text = get_ip_ru_content(),
+    Regex = "(\\d{1,3}\\.){3}\\d{1,3}",
+    {match, Result} = re:run(Text, get_mp(Regex, [unicode]), [global, {capture, first, list}]),
+    ?assertEqual(Expected, Result).
+	
+	
+-endif.
+-endif.
